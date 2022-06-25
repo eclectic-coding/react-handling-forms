fcb3b2d47202b8fbcf6b68e498602394557382da
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-06-25 13:00:22 -0400

    Set up basic form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -42,31 +42,32 @@ const SimpleInput = (props) => {
   const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
   const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
 
-  return (<form onSubmit={formSubmissionHandler}>
-    <div className={nameInputClasses}>
-      <label htmlFor="name">Your Name</label>
-      <input
-        type="text"
-        id="name"
-        onChange={nameChangedHandler}
-        onBlur={nameBlurHandler}
-        value={enteredName} />
-      {nameInputHasError && <p className="error-text">Name must not be empty</p>}
-    </div>
-    <div className={emailInputClasses}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        onChange={emailChangedHandler}
-        onBlur={emailBlurHandler}
-        value={enteredEmail} />
-      {emailInputHasError && <p className="error-text">Email must not be empty</p>}
-    </div>
-    <div className="form-actions">
-      <button disabled={!formIsValid}>Submit</button>
-    </div>
-  </form>);
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangedHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName} />
+        {nameInputHasError && <p className="error-text">Name must not be empty</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail} />
+        {emailInputHasError && <p className="error-text">Email must not be empty</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>);
 };
 
 export default SimpleInput;
